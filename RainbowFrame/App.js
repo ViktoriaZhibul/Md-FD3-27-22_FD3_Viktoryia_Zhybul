690ec2eb873c45ed094e468b3a7df3ad13815075
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2022-11-09 16:46:20 +0300

    RaibowFrame ready to check

---
+++ RainbowFrame/App.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+class App extends React.Component {
+
+  render() {
+      let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+      return (
+          <RainbowFrame colors={colors}>
+              Hello!
+          </RainbowFrame>
+      );
+  }
+}
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('container')
+);
+
+export default App;
+
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+    const getReduce = (children, current) => {
+        return (
+            <div style={{ border: "solid 10px " + current, padding: "5px" }}>
+                {children}
+            </div>
+        );
+    }
+
+    const rainbowFrame = this.props.colors.reduce(getReduce, this.props.children);
+
+    return (
+        <div style={{ width: 800, textAlign: 'center', margin: 'auto' }}>
+            {rainbowFrame}
+        </div>
+    );
+}
+
+}
+
+export default RainbowFrame;
