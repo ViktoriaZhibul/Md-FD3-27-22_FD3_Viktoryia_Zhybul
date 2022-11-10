80ae5fa8ce58269899ff0a1584c02b36c17310ac
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2022-11-10 23:25:51 +0300

    br2jsx ready to check

---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class BR2JSX extends React.Component {
+
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+  
+  render() {
+    const regexp = /<br\s?\/?>/;
+    const textArray = this.props.text.split(regexp);
+
+    textArray.map((el, item)=>{textArray.splice(item*2+1, 0, <br key={el+item} />)});
+    textArray.splice(-1, 1);
+    console.log(textArray)
+    return (
+      <div className="br2jsx">{textArray}</div>
+    );
+  }
+
+}
+
+export default BR2JSX;
