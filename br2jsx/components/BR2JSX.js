30ad6d4484dbb33b239b326baff1b3fc63e9a6db
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2022-11-11 22:02:21 +0300

    br2jsx, first correction to check

--- br2jsx/components/BR2JSX.js
+++ br2jsx/components/BR2JSX.js
@@ -11,11 +11,16 @@ class BR2JSX extends React.Component {
     const regexp = /<br\s?\/?>/;
     const textArray = this.props.text.split(regexp);
 
-    textArray.map((el, item)=>{textArray.splice(item*2+1, 0, <br key={el+item} />)});
-    textArray.splice(-1, 1);
+    let result = [];
 
+    for (let i=0; i<textArray.length; i++) {
+      result.push(textArray[i]);
+      if(i != textArray.length-1) {
+        result.push(<br key={10+i}/>);
+      }
+    }
     return (
-      <div className="br2jsx">{textArray}</div>
+      <div className="br2jsx">{result}</div>
     );
   }
 }
